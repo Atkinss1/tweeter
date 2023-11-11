080daad9c72e0f97fc4d423f5c95896d53ec266c
Atkinss1 <jordanatkins@live.com>
2023-11-11 13:28:55 -0700

    added: JQuery syntax

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
   $("#tweet-text").on('input', function() {
-    let textValue = $(this).val();
-    let currentTextLength = textValue.length;
-    const remainingChars = 140 - currentTextLength;
+    let $textValue = $(this).val();
+    let $currentTextLength = $textValue.length;
+    const remainingChars = 140 - $currentTextLength;
     
-    let counterElement = $(this).siblings('.button-counter').find('.counter');
+    let $counterElement = $(this).siblings('.button-counter').find('.counter');
 
-    counterElement.text(remainingChars);
+    $counterElement.text(remainingChars);
 
     if (remainingChars < 0) {
-      counterElement.css('color', 'rgb(209, 73, 10)');
-      counterElement.css('animation', 'button 1s ease-out infinite');
+      $counterElement.css('color', 'rgb(209, 73, 10)');
+      $counterElement.css('animation', 'button 1s ease-out infinite');
     } else {
-      counterElement.css('color', '');
-      counterElement.css('animation-play-state', 'paused');
+      $counterElement.css('color', '');
+      $counterElement.css('animation-play-state', 'paused');
     }
 
   });
