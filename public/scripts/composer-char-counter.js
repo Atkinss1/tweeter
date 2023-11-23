40e8d61e83f6e5f4d5f92c423d1d226f5ad3cc78
Atkinss1 <jordanatkins@live.com>
2023-11-23 16:30:55 -0700

    refactor: jQuery variable

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,13 +2,14 @@ $(document).ready(function() {
   $('.tweet-text').on('input', function() {
     let $textValue = $(this).val();
     let $currentTextLength = $textValue.length;
-    const remainingChars = 140 - $currentTextLength;
+    const $maxCharLength = 140;
+    const $remainingChars = $maxCharLength - $currentTextLength;
     
     let $counterElement = $(this).siblings('.button-counter').find('.counter');
 
-    $counterElement.text(remainingChars);
+    $counterElement.text($remainingChars);
 
-    if (remainingChars < 0) {
+    if ($remainingChars < 0) {
       $counterElement.css('color', 'rgb(209, 73, 10)');
       $counterElement.css('animation', 'button 1s ease-out infinite');
     } else {
